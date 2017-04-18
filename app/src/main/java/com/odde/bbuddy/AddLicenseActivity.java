005c441acf2466fc7da5240954f989b6df9d0bac
ChiangnanLin <chiangnanlin@kkbox.com>
2017-04-18 17:02:49 +0800

    Add AddLicenseActivity UI without implementation

--- app/src/main/java/com/odde/bbuddy/AddLicenseActivity.java
+++ app/src/main/java/com/odde/bbuddy/AddLicenseActivity.java
@@ -1,10 +1,17 @@
 package com.odde.bbuddy;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 
 public class AddLicenseActivity extends AppCompatActivity {
 
 
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
+		setContentView(R.layout.activity_add_license);
+	}
 }
