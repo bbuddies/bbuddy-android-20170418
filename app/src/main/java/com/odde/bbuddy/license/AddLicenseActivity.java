f00fd0ffcb93fd40ba89e79d0cda53af88ba1894
Aaron Chu <aaronchu@kkstream.com.tw>
2017-04-18 18:25:57 +0800

    add license: add license button logic without sending request to backend

--- app/src/main/java/com/odde/bbuddy/license/AddLicenseActivity.java
+++ app/src/main/java/com/odde/bbuddy/license/AddLicenseActivity.java
@@ -6,15 +6,29 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.odde.bbuddy.R;
 
+import org.robobinding.ViewBinder;
+
+import javax.inject.Inject;
+
+import static com.odde.bbuddy.di.component.ActivityComponentFactory.createActivityComponentBy;
+
 /**
  * Created by aaronchu on 2017/4/18.
  */
 
 public class AddLicenseActivity extends AppCompatActivity {
 
+	@Inject
+	EditableLicense mLicense;
+
+	@Inject
+	ViewBinder mViewBinder;
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_license);
+		createActivityComponentBy(this).inject(this);
+		setContentView(mViewBinder.inflateAndBind(R.layout.activity_add_license, mLicense));
 	}
+
 }
