a394a958ee87a888c73d72d63e896255a729c7ab
ChiangnanLin <chiangnanlin@kkbox.com>
2017-04-19 11:43:26 +0800

    Miss add unit test

--- app/src/test/java/com/odde/bbuddy/license/viewmodel/AddLicenseControllerTest.java
+++ app/src/test/java/com/odde/bbuddy/license/viewmodel/AddLicenseControllerTest.java
@@ -3,12 +3,16 @@ package com.odde.bbuddy.license.viewmodel;
 
 import com.odde.bbuddy.license.api.LicenseApi;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class AddLicenseControllerTest {
@@ -25,5 +29,53 @@ public class AddLicenseControllerTest {
 		verify(api).addLicense(captor.capture());
 		assertThat(captor.getValue().getAmount()).isEqualToIgnoringCase("9487");
 		assertThat(captor.getValue().getMonth()).isEqualToIgnoringCase("2017-04");
+	}
+
+	@Test
+	public void add_should_show_toast_when_amount_empty() {
+		LicenseApi api = mock(LicenseApi.class);
+		AddLicenseController controller = Mockito.spy(new AddLicenseController(api));
+
+		int timesShowToast = 0;
+		int timesCheckZero = 0;
+
+		Mockito.doNothing().when(controller).showAmountZeroToast();
+		++timesShowToast;
+
+		controller.showAmountZeroToast();
+		controller.add();
+
+
+		verify(controller, times(++timesCheckZero)).isAmountZeroEmpty();
+		verify(controller, times(++timesShowToast)).showAmountZeroToast();
+
+		controller.setAmount("0");
+		controller.add();
+
+		verify(controller, times(++timesCheckZero)).isAmountZeroEmpty();
+		verify(controller, times(++timesShowToast)).showAmountZeroToast();
+
+		controller.setAmount("100");
+		controller.add();
+
+		verify(controller, times(++timesCheckZero)).isAmountZeroEmpty();
+		verify(controller, times(timesShowToast)).showAmountZeroToast();
+		ArgumentCaptor<License> captor = forClass(License.class);
+		verify(api).addLicense(captor.capture());
+	}
+
+	@Test
+	public void isAmountZeroEmpty() {
+		LicenseApi api = mock(LicenseApi.class);
+		AddLicenseController controller = new AddLicenseController(api);
+
+		// default empty
+		Assert.assertTrue(controller.isAmountZeroEmpty());
+
+		controller.setAmount("9487");
+		Assert.assertFalse(controller.isAmountZeroEmpty());
+
+		controller.setAmount("0");
+		Assert.assertTrue(controller.isAmountZeroEmpty());
 	}
 }
